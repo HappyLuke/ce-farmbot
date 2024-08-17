27d2990ec356edd92b487c3bca2be4a45ba33a17
LukasHappe <lukas.m.happe@gmail.com>
2024-08-17 22:32:42 +0200

    add more explanations

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # Farming Bot for Castle Empire
 
-Bot is made for a 1920x1080 screen. Start by being inside the castle and have all rooms of resource types centered in a 4x4 square.
-Bot is switching between two characters all the time.
+Bot is made for a 1920x1080 screen. Start by being inside the castle and have all rooms of resource types centered in a 4x4 square. (look in image/image.png for an example)
+Bot is switching between two characters all the time. Check the playback.py script for more information.
 
 Any improvements are welcome.
 
 recorder.py is used for recording
 
 playack.py is used for action.
+Start bot:
+py -u ./bot/playback.py
 
 Working and tested on Python 3.12
 
--- bot/playback.py
+++ bot/playback.py
@@ -5,26 +5,43 @@ import json
 import random
 
 
+
+# make sure to place the resource rooms correctly for collections
+# you must have two characters in your account
+# start script with py -u ./bot/playpback.py
+# tested on python 3.12
 def main():
     initializePyAutoGUI()
     index = 0
-    change = [] #['happy', 'jluke']
+    # you can define to switch between two accounts. this probably needs to be adjusted by yourself.
+    # I advise just use two characters for this bot and just switch between them
+    change = [] #['google_first', 'google_second']
+    # defining an array of resources to gather from. one of them will be choosen randomly.
+    # so if you add one type more often then others, it is more likely to gather this one
+    rss = ['gp', 'gp', 'gp', 'gp', 'gp', 'food', 'food', 'iron', 'iron', 'iron', 'iron', 'wood', 'wood']
     countdownTimer()
+    # running forever
     while True:
-        do_all()
+        # collect, recruit, help, rally and gather
+        do_all(rss)
+        # switch to the other character in this account
         switch_char()
-        do_all()
+        # collect, recruit, help, rally and gather
+        do_all(rss)
+        # if you defined the change array, change account.
         if change:
             change_acc(change[index])
             index += 1
             if index >= len(change):
                 index = 0
         else:
+            # otherwise switch back to the other character and start again
             switch_char()
     
     print("Done")
 
-def do_all():
+# collect rss, recruit if free, help members if no rally, rally if rally available and you have enough ap (only the first one), send all marches gathering
+def do_all(rss):
     if checkColor(977, 531) not in [(0, 0, 0), (1, 1, 1)]:
         playActions('general/exit_enter_castle.json')
         sleep(2.00)
@@ -32,7 +49,6 @@ def do_all():
     recruit()
     help()
     rally()
-    rss = ['gp', 'gp', 'gp', 'gp', 'gp', 'food', 'food', 'iron', 'iron', 'iron', 'wood', 'wood']
     success = True
     while success:
         index = int(random.random() * len(rss))
@@ -96,11 +112,11 @@ def switch_char():
     ad_check()
 
 def ad_check():
-    if checkColor(1806, 113) == (246, 245, 245) or checkColor(1770, 128) == (246, 245, 245):
+    if checkColor(1806, 113) == (246, 245, 245):
         playActions("general/remove_ad.json")
     elif checkColor(1839, 73) == (246, 245, 245):
         playActions("general/remove_ad_2.json")
-    elif checkColor(1772, 128) == (246, 245, 245):
+    elif checkColor(1772, 128) == (246, 245, 245) or checkColor(1771, 126) == (246, 245, 245):
         playActions("general/remove_ad_3.json")
     sleep(2.00)
 
